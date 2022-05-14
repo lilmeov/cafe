3a2e93dde8022d2016ee7ce141b191a789627945
Nag1ma <nagima242424@gmail.com>
2022-05-14 11:09:45 +0600

    correcting cards

--- src/main/java/com/example/cafe/cafe/CafeController.java
+++ src/main/java/com/example/cafe/cafe/CafeController.java
@@ -3,6 +3,7 @@ package com.example.cafe.cafe;
 import com.example.cafe.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,10 +17,11 @@ import java.util.List;
 public class CafeController {
     private final CafeService cafeService;
 
-    private final CafeRepository cafeRepository;
 
     @GetMapping("/mainPage")
-    public String giveCafeListPage(){
+    public String giveCafeListPage(Model model){
+        List<Cafe> cafes = cafeService.getAllCafes();
+        model.addAttribute("cafes", cafes);
         return "list-cafe";
     }
 }
--- src/main/java/com/example/cafe/cafe/CafeService.java
+++ src/main/java/com/example/cafe/cafe/CafeService.java
@@ -3,9 +3,14 @@ package com.example.cafe.cafe;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CafeService {
     private final CafeRepository cafeRepository;
 
+    public List<Cafe> getAllCafes(){
+        return cafeRepository.findAll();
+    }
 }
